1e4a472a7aaf34494086cb53782374618a44a38c
err931 <119707793+err931@users.noreply.github.com>
2023-01-01 00:00:00 +0900

    style: 💄 Rename file to kebab-case

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 // This file is based on textlint-plugin-markdown (Author: azu)
-import { XLIFFProcessor } from "./XLIFFProcessor";
+import { XLIFFProcessor } from "./xliff-processor";
 
 export default {
   Processor: XLIFFProcessor,
